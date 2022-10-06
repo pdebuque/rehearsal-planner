6048919b842d3c556286b86e5e64540d3da25d9e
Paolo Debuque <pdebuque@gmail.com>
2022-10-06 11:38:40 -0500

    linked jquery-ui

--- script.js
+++ script.js
@@ -8,17 +8,46 @@ function readyNow() {
     $('#sectionButton').on('click', addSection)
 }
 
-function setUpRehearsal() {
-    //take inputs above and deliver them as a header underneath the questionaire
-    console.log('in setUpRehearsal')
+let sectionStart = 0;
 
-    $('body').append(
-        `<div class="generalInfo"><h3>Rehearsal Number ${$('#rehearsalNumber').val()}</h3>
+function setUpRehearsal() {
+    //this should only work if there is not already a general info section
+    if (!$('#rehearsalHeader').html()) {
+
+        //take inputs above and deliver them as a header underneath the questionaire
+        console.log('in setUpRehearsal')
+
+        $('#rehearsalHeader').html(
+            `<div class="generalInfo">
+        <h3>${$('#ensembleName').val()}
+        <h4>Rehearsal ${$('#rehearsalNumber').val()}</h4>
         <div> ${$('#rehearsalDate').val()} </div>
         <div> ${$('#rehearsalStart').val()}-${$('#rehearsalEnd').val()} </div>
-    </div>`)
+    </div>`);
+
+        //update section start time for use in section addition
+
+        sectionStart = $('#rehearsalStart').val();
+
+        //clear inputs
+        $('#ensembleName').val('');
+        $('#rehearsalNumber').val('');
+        $('#rehearsalDate').val(null);
+        $('#rehearsalStart').val(null);
+        $('#rehearsalEnd').val(null);
+    }
 }
 
 function addSection() {
-    console.log('in addSection')
+    console.log('in addSection');
+
+    $('#rehearsalSections').html(
+        `<div class='rehearsalSection'>`
+    )
+
+
+    //update section start
+    sectionStart += $('#sectionLength').val()
+
+    // clear inputs
 }
